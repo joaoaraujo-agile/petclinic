c750b2ebc400f1d75da26631e914cdee099482fe
João Paulo de Carvalho Araújo <joao.araujo@agilesolutions.com>
2021-05-13 16:42:14 -0300

    Fix: UserResourceTest imports refactored

--- src/test/java/com/agile/petclinic/resources/UserResourceTest.java
+++ src/test/java/com/agile/petclinic/resources/UserResourceTest.java
@@ -9,8 +9,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
