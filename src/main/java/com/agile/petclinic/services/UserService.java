0316e933113ed94d994171f5faa39a92a68868ec
João Paulo de Carvalho Araújo <joao.araujo@agilesolutions.com>
2021-05-13 01:28:00 -0300

    Feature: Change in expiration of JWT to 24h; UserService encoding updated passwords

--- src/main/java/com/agile/petclinic/services/UserService.java
+++ src/main/java/com/agile/petclinic/services/UserService.java
@@ -46,7 +46,7 @@ public class UserService {
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
-		entity.setPassword(obj.getPassword());		
+		entity.setPassword(pe.encode(obj.getPassword()));		
 	}
 	
 	public static UserSS authenticated() {
